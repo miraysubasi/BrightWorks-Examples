939a0d58cd471a89ab83cdf796a26c5f25dd4498
miraysubasi <141759975+miraysubasi@users.noreply.github.com>
2023-09-11 20:29:51 +0300

    Create app.js

---
+++ week04/29.08.2023/02_DOM/02_single_element_select/app.js
@@ -0,0 +1,16 @@
+let sonuc;
+//sonuc = document.getElementById("card-header");//idye göre element secer
+//sonuc.style.fontSize="deneme";
+//sonuc.style.backgroundColor="#FCF050";
+//sonuc.className = "bg-warning";
+// sonuc = document.getElementById("title").classList;
+// sonuc = document.getElementById("bau-bright").style.display="none";
+
+// sonuc = document.getElementById("title");
+// id li class lı element i queryselector getirir.
+sonuc = document.querySelector("#card-header");
+sonuc = document.querySelector(".display-6");
+sonuc = document.querySelector(".card-title");
+//sonuc = documant.querySelector("ul");
+
+console.log(sonuc);
