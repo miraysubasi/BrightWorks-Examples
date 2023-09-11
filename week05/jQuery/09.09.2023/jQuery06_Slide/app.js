bb1caba16c17589fd5c7c8df2ee5c67d55279ae9
miraysubasi <141759975+miraysubasi@users.noreply.github.com>
2023-09-11 20:33:20 +0300

    Create app.js

---
+++ week05/jQuery/09.09.2023/jQuery06_Slide/app.js
@@ -0,0 +1,9 @@
+$("#btn-slide-up").click(function () {
+    $("#box").slideUp(1000);
+})
+$("#btn-slide-down").click(function () {
+    $("#box").slideDown(1000);
+})
+$("#btn-slide-toggle").click(function () {
+    $("#box").slideToggle(1000);
+})
