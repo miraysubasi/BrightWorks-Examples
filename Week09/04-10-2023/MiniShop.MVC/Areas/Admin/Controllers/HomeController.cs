4b5695fcf7f500d18e2cd98ef5ead91937bc03a8
miraysubasi <141759975+miraysubasi@users.noreply.github.com>
2023-10-04 21:42:53 +0300

    4-10-2023
    wissen

---
+++ Week09/04-10-2023/MiniShop.MVC/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MiniShop.MVC.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
